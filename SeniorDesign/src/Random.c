9a15884fac4310583cc746845bbe5ef1a566d9c2
CelesAmatsu <phillipm91@knights.ucf.edu>
2017-03-20 20:04:46 -0400

    Updated RNG
    Better RNG, Seed Var added, Prescaled RNG added

--- SeniorDesign/src/Random.c
+++ SeniorDesign/src/Random.c
@@ -1,9 +1,38 @@
 #include "Random.h"
 #include <stdlib.h>
 
+
+#define MULT 31821  // only X-X-Even-2-1 allowed
+#define INC 13849
+
+unsigned int SEED = 241;
+unsigned int current =0;
+
+
 unsigned int getRandom() {
-	return rand();
+    current=SEED * MULT + INC % 65536;   //0-2^16 can be adjusted for wider range
+    SEED = current;
+    return current;
 }
+
+void updateSEED(unsigned int s){ //re-seed with only 1-64k
+    SEED = s;
+}
+
+unsigned int getRandomLow(){  //0-2^8
+    return (getRandom() & 0x00FF);
+}
+
+unsigned int getRandomLowExtend(){ //0-2^9
+    unsigned int i= getRandomLow();
+    if(i<256){
+        return(i);
+    }
+    else{
+        return(i*2);
+    }
+}
+
 
 Pixel getRandomPixel() {
 	Pixel p;
@@ -12,3 +41,4 @@ Pixel getRandomPixel() {
 	p.B = rand();
 	return p;
 }
+
--- SeniorDesign/src/Random.h
+++ SeniorDesign/src/Random.h
@@ -4,6 +4,12 @@
 
 // Get a random integer
 extern unsigned int getRandom();
+// Get a random integer 0-2^8
+extern unsigned int getRandomLow();
+// Get a random integer 0-2^9
+extern unsigned int getRandomLowExtend();
+// Update SEED do not use 0
+extern void updateSEED(unsigned int);
 // Get a color with a random R, G, B value
 extern Pixel getRandomPixel();
 #endif
