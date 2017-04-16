5a94cd2a99f29d1d92ebc725320575592d44062b
CelesAmatsu <phillipm91@knights.ucf.edu>
2017-04-16 16:30:17 -0400

    cloud app fin

--- SeniorDesign/src/App_Cloud.c
+++ SeniorDesign/src/App_Cloud.c
@@ -3,12 +3,23 @@
 #include "Noise.h"
 #include "Input.h"
 
+#define LEFT_INPUT     0x04
+#define RIGHT_INPUT    0x08
+
+#define DIRECTION          0x0F
+#define DIRECTION_UP       2
+#define DIRECTION_LEFT     3
+#define DIRECTION_DOWN     0
+#define DIRECTION_RIGHT    1
+
 struct appData
 {
    int           frame;
-   int           frame2;
+   int           base;
+   int           size;
+   int           hue;
    unsigned char flag;
-   unsigned char fadeoutFlag;
+
 }
 
 typedef   appData;
@@ -20,7 +31,7 @@ void App_Cloud_Init(void);
 void App_Cloud_Tick(void);
 void App_Cloud_Deinit(void);
 void App_Cloud_New(App *app);
-
+void errorCheck();
 
 void Demo_Cloud_Init(void)
 {
@@ -57,21 +68,90 @@ void Demo_Cloud_Deinit(void)
 void App_Cloud_Init(void)
 {
    appCloud_Data              = &AppStorage;
-   appCloud_Data->frame       = 240;
+   appCloud_Data->frame       = 0;
    appCloud_Data->flag        = 0;
+   appCloud_Data->hue       = 0;
+   appCloud_Data->base        = 5;
+   appCloud_Data->size        = 3;
+
 
 }
 
 
 void App_Cloud_Tick(void)
 {
-
+    generateNoise();
+    if (Input_Tap)
+          {
+              if (Input_Tap & UP_INPUT)
+               {
+                  appCloud_Data->base+=5;
+               }
+               else if (Input_Tap & DOWN_INPUT)
+               {
+                   appCloud_Data->base-=5;
+               }
+               else if (Input_Tap & LEFT_INPUT)
+               {
+                   appCloud_Data->hue-=5;
+               }
+               else if (Input_Tap & RIGHT_INPUT)
+               {
+                   appCloud_Data->hue+=5;
+               }
+             if (Input_Tap & B_INPUT)
+             {
+                 appCloud_Data->size--;
+             }
+             if (Input_Tap & A_INPUT)
+             {
+                 appCloud_Data->size++;
+             }
+          }
+          if (User2_Input_Tap)
+          {
+              if (User2_Input_Tap & UP_INPUT)
+               {
+                  appCloud_Data->base+=5;
+               }
+               else if (User2_Input_Tap & DOWN_INPUT)
+               {
+                   appCloud_Data->base-=5;
+               }
+               else if (User2_Input_Tap & LEFT_INPUT)
+               {
+                   appCloud_Data->hue-=5;
+               }
+               else if (User2_Input_Tap & RIGHT_INPUT)
+               {
+                   appCloud_Data->hue+=5;
+               }
+             if (User2_Input_Tap & B_INPUT)
+             {
+                 appCloud_Data->size--;
+             }
+             if (User2_Input_Tap & A_INPUT)
+             {
+                 appCloud_Data->size++;
+             }
+          }
+          Input_Tap &= ~(LEFT_INPUT | RIGHT_INPUT | UP_INPUT | DOWN_INPUT | A_INPUT | B_INPUT);
+          User2_Input_Tap &= ~(LEFT_INPUT | RIGHT_INPUT | UP_INPUT | DOWN_INPUT | A_INPUT | B_INPUT);
+          errorCheck();
+          drawCloudColorRotate(appCloud_Data->hue,appCloud_Data->size,appCloud_Data->base);
 }
 
 
 void App_Cloud_Deinit(void)
 {
+    int i;
 
+    for (i = 0; i < sizeof(appData); i++)
+    {
+       AppStorage[i] = 0;
+    }
+
+    appCloud_Data = 0;
 }
 
 
@@ -83,4 +163,32 @@ void App_Cloud_New(App *app)
    app->App_Init    = &App_Cloud_Init;
    app->App_Tick    = &App_Cloud_Tick;
    app->App_Deinit  = &App_Cloud_Deinit;
+}
+
+void errorCheck()
+{
+    if(appCloud_Data->base>512)
+    {
+        appCloud_Data->base=0;
+    }
+    if(appCloud_Data->base<0)
+    {
+        appCloud_Data->base=0;
+    }
+    if(appCloud_Data->hue>360)
+    {
+        appCloud_Data->hue=0;
+    }
+    if(appCloud_Data->hue<0)
+    {
+        appCloud_Data->hue=360;
+    }
+    if(appCloud_Data->size>512)
+    {
+        appCloud_Data->size=0;
+    }
+    if(appCloud_Data->size<0)
+    {
+        appCloud_Data->size=0;
+    }
 }
